fc9356e9b2737cbca98d6074d75bfc0f26aae915
Joel Costa <joeldavidcostajr@gmail.com>
2023-07-03 21:10:55 -0300

    Commit inicial

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("# Rinha de academia!!!")
+st.write("## Quem ganhará um rodízio?")
+
+# st.button("Registrar presença", on_click=registra_presenca)
+
+# def registra_presenca():
